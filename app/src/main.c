d1e0f9734922182a7511a19b5353db548a871d70
john-sperida <j.esperida49@gmail.com>
2025-10-02 19:34:19 -0600

    Initiaized Pin

--- app/src/main.c
+++ app/src/main.c
@@ -1,7 +1,24 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+#define LED0_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 int main(void) {
+    int ret;
+
+    //looks like we are checking if it's ready
+    if(!gpio_is_ready_dt(&led0)) {
+        return -1;
+    }
+
+    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+    
+    //error catching?
+    if (ret < 0) {
+        return ret;
+    }
 
     while (1) {
 
