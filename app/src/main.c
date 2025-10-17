01cadbfcdfc4dfb75d9c9aae678177669b102284
john-sperida <j.esperida49@gmail.com>
2025-10-16 19:20:25 -0600

    Just started on main

--- app/src/main.c
+++ app/src/main.c
@@ -2,45 +2,34 @@
  * main.c
  */
 
-#include <stdio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS 1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-int main(void)
-{
+#define SW0_NODE DT_ALIAS(sw0)
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+int main(void) {
     int ret;
-    bool led_state = true;
+    if (lgpio_is_ready_dt(&button)){
+        return 0;
+    }
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 
-    if (!gpio_is_ready_dt(&led)) {
+    //note the Yoda case prevents (yoda == 5)
+    //from being (yoda = 5)
+    if (0 > ret) {
         return 0;
     }
 
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return 0;
+
+    while(1) {
+
     }
 
-    while (1) {
-        ret = gpio_pin_toggle_dt(&led);
-        if (ret < 0) {
-            return 0;
-        }
-
-        led_state = !led_state;
-        printf("LED state: %s\n", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
-    }
     return 0;
 }
+
