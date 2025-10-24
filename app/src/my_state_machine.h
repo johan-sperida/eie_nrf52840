c6b120d84968bafc83841d29db3715f92192ced5
john-sperida <j.esperida49@gmail.com>
2025-10-23 19:52:45 -0600

    DONE!

---
+++ app/src/my_state_machine.h
@@ -0,0 +1,11 @@
+/** 
+ * @file  my_state_machine.h
+ * */ 
+
+#ifndef MY STATE MACHINE W
+#define MY_STATE MACHINE H
+
+void state_machine_init();
+
+int state_machine_run();
+#endif // MY STATE MACHINE H
