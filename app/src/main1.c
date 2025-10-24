4fafcf62ef09afecde0c028502184551c23b9d5c
john-sperida <j.esperida49@gmail.com>
2025-10-23 19:03:08 -0600

    Caught up! (technically)

---
+++ app/src/main1.c
@@ -0,0 +1,65 @@
+/*
+ * main.c1
+ */
+
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+
+#define SLEEP_TIME_MS 1000
+
+#define SW0_NODE DT_ALIAS(sw0)
+
+//struct pointer for the device that triggered, bundles relevant callback data, pin that caused
+void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+    printk("Button 0 pressed! Banana\n");
+}
+//make a struct instance button
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+static struct gpio_callback button_isr_data;
+
+int main(void) {
+    int ret;
+
+
+    //check if ready
+    if (!gpio_is_ready_dt(&button)){
+        return 0;
+    }
+
+    //configure
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    //note the Yoda case prevents (yoda == 5)
+    //from being (yoda = 5)
+    if (0 > ret) {
+        return 0;
+    }
+
+    //configure for interrupt
+    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+    if (0 > ret) {
+    return 0;
+    }
+
+    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
+    gpio_add_callback(button.port, &button_isr_data);
+
+
+
+    while(1) {
+        
+        //monkey method (always check)
+        // ret = gpio_pin_get_dt(&button);
+        // if (0 < ret) {
+        //     printk("Bonanza!\n");
+        // }
+        // k_msleep(SLEEP_TIME_MS);
+    }
+
+    return 0;
+}
+
